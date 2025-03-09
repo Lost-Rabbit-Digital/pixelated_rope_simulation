d9ff9b0a138efa76b638de102df1c32119131601
Boden McHale <bodenkaikarden@gmail.com>
2025-03-09 11:35:31 -0400

    Revert "Migrated to ECS architecture"
    This reverts commit 6dc20de9b67f91ceee9afe50d9ca8e42312c130d.

--- godot_project/addons/pixel_rope/scripts/pixel_rope.gd
+++ godot_project/addons/pixel_rope/scripts/pixel_rope.gd
@@ -12,18 +12,6 @@ const rope_node_script = preload("res://addons/pixel_rope/scripts/components/rop
 const rope_anchor_script = preload("res://addons/pixel_rope/scripts/components/rope_anchor.gd")
 const line_algorithms = preload("res://addons/pixel_rope/scripts/utils/line_algorithms.gd")
 
-# Preload components and systems
-const rope_segment_script = preload("res://addons/pixel_rope/scripts/components/rope_segment.gd")
-const rope_collision_script = preload("res://addons/pixel_rope/scripts/components/rope_collision.gd")
-const verlet_system_script = preload("res://addons/pixel_rope/scripts/systems/physics/verlet_system.gd")
-const constraint_system_script = preload("res://addons/pixel_rope/scripts/systems/physics/constraint_system.gd")
-const collision_system_script = preload("res://addons/pixel_rope/scripts/systems/physics/collision_system.gd")
-const line_renderer_script = preload("res://addons/pixel_rope/scripts/systems/rendering/line_renderer.gd")
-const debug_renderer_script = preload("res://addons/pixel_rope/scripts/systems/rendering/debug_renderer.gd")
-const anchor_interaction_script = preload("res://addons/pixel_rope/scripts/systems/interaction/anchor_interaction.gd")
-const rope_interaction_script = preload("res://addons/pixel_rope/scripts/systems/interaction/rope_interaction.gd")
-const signal_manager_script = preload("res://addons/pixel_rope/scripts/utils/signal_manager.gd")
-
 # Editor gizmo and selection tracking
 var _editor_selection: EditorSelection
 var _current_selected_rope: PixelRope = null
